2e575eb4f9a0786a558a4cc9f54d2de27ccbbb8d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-16 13:52:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,12 @@ st.title('🎈 Animated GIF Maker')
 uploaded_file = st.file_uploader("Choose a file", type=['mov', 'mp4'])
 
 if uploaded_file is not None:
+  # Save to temp file
+  tfile = tempfile.NamedTemporaryFile(delete=False) 
+  tfile.write(f.read())
+  # Open file
   clip = VideoFileClip(uploaded_file)
+  # Display width of video
   st.write(clip.w)
 else:
   st.warning('☝️ Upload a video file')
