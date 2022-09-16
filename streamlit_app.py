1a618525049b87d85b828e8c3a1b94d4acc544ea
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-16 19:25:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ if uploaded_file is not None:
   clip.save_frame('frame.jpg', t=selected_frame)
   frame_image = Image.open("frame.jpg")
   st.image(frame_image)
+  st.write(frame_image.size)
   
 else:
   st.warning('👈 Upload a video file')
