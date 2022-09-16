8b1e222a33fad9f5acdbce4fe1f04247bb002d87
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-16 13:58:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,15 @@ if uploaded_file is not None:
   # Save to temp file
   tfile = tempfile.NamedTemporaryFile(delete=False) 
   tfile.write(uploaded_file.read())
+  
   # Open file
   clip = VideoFileClip(tfile.name)
-  # Display width of video
-  st.write(clip.w)
+  
+  # Display output
+  col1, col2, col3 = st.columns(3)
+  col1.metric('Width', clip.w, '')
+  col2.metric('Height', clip.h, '')
+  col3.metric('Duration', clip.duration, '')
+  
 else:
   st.warning('☝️ Upload a video file')
