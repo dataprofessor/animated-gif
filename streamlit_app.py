729eb5161b37ec2454f47cb6eca417f345217d7c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-16 14:15:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,24 +4,24 @@ from moviepy.editor import VideoFileClip
 
 st.title('🎈 Animated GIF Maker')
 
-with st.form('my_form'):
+with st.sidebar.form('my_form'):
   st.sidebar.header('Upload file')
   uploaded_file = st.sidebar.file_uploader("Choose a file", type=['mov', 'mp4'])
   submitted = st.sidebar.form_submit_button('Submit')
 
-  if submitted:
-    # Save to temp file
-    tfile = tempfile.NamedTemporaryFile(delete=False) 
-    tfile.write(uploaded_file.read())
-
-    # Open file
-    clip = VideoFileClip(tfile.name)
-
-    # Display output
-    col1, col2, col3 = st.columns(3)
-    col1.metric('Width', clip.w, 'pixels')
-    col2.metric('Height', clip.h, 'pixels')
-    col3.metric('Duration', clip.duration, 'seconds')
+if submitted:
+  # Save to temp file
+  tfile = tempfile.NamedTemporaryFile(delete=False) 
+  tfile.write(uploaded_file.read())
   
-  else:
-    st.warning('☝️ Upload a video file')
+  # Open file
+  clip = VideoFileClip(tfile.name)
+  
+  # Display output
+  col1, col2, col3 = st.columns(3)
+  col1.metric('Width', clip.w, 'pixels')
+  col2.metric('Height', clip.h, 'pixels')
+  col3.metric('Duration', clip.duration, 'seconds')
+  
+else:
+  st.warning('☝️ Upload a video file')
