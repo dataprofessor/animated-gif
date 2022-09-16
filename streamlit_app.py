1fde2df21f4dbb4d1f117504202d04864f0d9159
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-16 14:00:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from moviepy.editor import VideoFileClip
 
 st.title('🎈 Animated GIF Maker')
 
+st.sidebar.header('Upload file')
 uploaded_file = st.file_uploader("Choose a file", type=['mov', 'mp4'])
 
 if uploaded_file is not None:
