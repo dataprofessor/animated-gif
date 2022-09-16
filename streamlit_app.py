d9f57f95943cee3a4c92bab81b4fdcb0215d5277
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-16 14:57:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,15 +17,17 @@ if uploaded_file is not None:
   # Open file
   clip = VideoFileClip(tfile.name)
   
+  frames_count = int(clip.duration * clip.fps)
+  
   # Display output
   col1, col2, col3, col4, col5 = st.columns(5)
   col1.metric('Width', clip.w, 'pixels')
   col2.metric('Height', clip.h, 'pixels')
   col3.metric('Duration', clip.duration, 'seconds')
   col4.metric('FPS', clip.fps, '')
-  col5.metric('Frames', clip.duration * clip.fps, 'frames')
+  col5.metric('Frames', frames_count, 'frames')
   
-  st.slider('Select a frame', 1, clip.duration * clip.fps, 10)
+  st.slider.slider('Select a frame', 1, frames_count, 10)
   
   clip.save_frame('frame.jpg', t=10)
   frame_image = Image.open("frame.jpg")
