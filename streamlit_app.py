878dec61096540bc7dc1c84b90ef7fc21d5199a6
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-16 19:45:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,17 @@ from PIL import Image
 import numpy as np
 from moviepy.editor import VideoFileClip
 
+if 'clip_width' not in st.session_state:
+    st.session_state.clip_width = 0
+if 'clip_height' not in st.session_state:
+    st.session_state.clip_height = 0
+if 'clip_duration' not in st.session_state:
+    st.session_state.clip_duration = 0
+if 'clip_fps' not in st.session_state:
+    st.session_state.clip_fps = 0
+if 'clip_total_frames' not in st.session_state:
+    st.session_state.clip_total_frames = 0  
+    
 st.title('🎈 Animated GIF Maker')
 
 
@@ -18,15 +29,21 @@ if uploaded_file is not None:
   # Open file
   clip = VideoFileClip(tfile.name)
   
+  st.session_state.clip_width = clip.w
+  st.session_state.clip_height = clip.h
+  st.session_state.clip_duration = clip.duration
+  st.session_state.clip_fps = clip.fps
+  st.session_state.clip_total_frames = clip.duration * clip.fps
+  
   # Display output
   col1, col2, col3, col4, col5 = st.columns(5)
-  col1.metric('Width', clip.w, 'pixels')
-  col2.metric('Height', clip.h, 'pixels')
-  col3.metric('Duration', clip.duration, 'seconds')
-  col4.metric('FPS', clip.fps, '')
-  col5.metric('Frames', clip.duration * clip.fps, 'frames')
+  col1.metric('Width', st.session_state.clip_width, 'pixels')
+  col2.metric('Height', st.session_state.clip_height, 'pixels')
+  col3.metric('Duration', st.session_state.clip_duration, 'seconds')
+  col4.metric('FPS', st.session_state.clip_fps, '')
+  col5.metric('Total Frames', st.session_state.clip_total_frames, 'frames')
   
-  selected_frame = st.sidebar.slider('Select a time frame (s)', 0, int(clip.duration), int(np.median(clip.duration)) )
+  selected_frame = st.sidebar.slider('Select a time frame (s)', 0, int(st.session_state.clip_duration), int(np.median(st.session_state.clip_duration)) )
   selected_resolution_scaling = st.sidebar.slider('Scaling of video resolution', 0.0, 1.0, 0.5 )
   
   # Resizing of video
