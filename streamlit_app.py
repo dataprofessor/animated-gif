c24d55dc52915ac3c0246e2a8dc7f570fb3d0c35
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-05 22:12:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ uploaded_file = st.sidebar.file_uploader("Choose a file", type=['mov', 'mp4'])
 st.sidebar.markdown('''
 [Download example file](https://github.com/dataprofessor/animated-gif/raw/master/example/streamlit-app-starter-kit-screencast.mov)
 
-Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)
+Made with ❤️ by Chanin ([Data Professor](https://youtube.com/dataprofessor))
 ''')
 
 ## Display gif generation parameters once file has been uploaded ##
